95f28b8ff18f1f0cdf25b18168e60b30add791a5
Wayland Chan <wayland.chan@gmail.com>
2019-08-28 03:11:44 +0800

    should be working but need to change for stable/nightly

--- .emacs.d/my-init/rr.el
+++ .emacs.d/my-init/rr.el
@@ -6,13 +6,25 @@
   :config
   (setq rust-format-on-save t))
 
-(use-package racer :ensure t)
+(use-package racer :ensure t
+  :config
+  (progn
+	(add-hook 'rust-mode-hook #'racer-mode) ;; Activate racer in rust buffers.
+	(add-hook 'racer-mode-hook #'eldoc-mode) ;; Shows signature of current function in minibuffer.
+	;; Rust completions with Company and Racer.
+	(add-hook 'racer-mode-hook #'company-mode)))
+
 (use-package company :ensure t
   :config
   (setq company-idle-delay 0.2)
-  (add-hook 'rust-mode-hook
+  (add-hook 'racer-mode-hook
     (lambda ()
       (setq-local company-tooltip-align-annotations t))))
+
+;; Enable Cargo minor mode
+(use-package cargo :ensure t
+  :config
+  (add-hook 'rust-mode-hook 'cargo-minor-mode))
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
@@ -20,26 +32,13 @@
 (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 ;; Flycheck-End.
 
-;; Rust programming language-Begin.
-;; See Flycheck for syntax checking.
-;; Racer package configuration. For rust programming language support.
 ;; Path to rust source.
-(setq racer-rust-src-path (concat (getenv "HOME") "/.rustup/toolchains/nightly-2019-07-14-x86_64-apple-darwin/lib/rustlib/src/rust/src"))
+(setq racer-rust-src-path (concat (getenv "HOME") "/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src"))
 ;; Racer bin path.
 (setq racer-cmd (concat (getenv "HOME") "/.cargo/bin/racer"))
-(add-hook 'rust-mode-hook #'racer-mode) ;; Activate racer in rust buffers.
-(add-hook 'racer-mode-hook #'eldoc-mode) ;; Shows signature of current function in minibuffer.
-
-;; Rust completions with Company and Racer.
-(add-hook 'racer-mode-hook #'company-mode)
-
-;; Enable Cargo minor mode
-(add-hook 'rust-mode-hook 'cargo-minor-mode)
 
 ;; Map TAB key to completions.
 (local-set-key (kbd "TAB") 'company-indent-or-complete-common)
 (setq company-tooltip-align-annotations t)
 
-;; Rust Formatter. Run rustfmt before saving rust buffers
-;;(add-hook 'rust-mode-hook #'rustfmt-enable-on-save)
 ;; Rust-End.
