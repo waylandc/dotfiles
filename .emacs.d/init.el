edda22c60fb8b9351af7025613e5abd655d3ff6f
waylandc <wayland.chan@gmail.com>
2020-11-25 21:59:02 +0800

    add some indentation stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -121,7 +121,39 @@
 (setq-default kill-whole-line t)  ; Kill line including '\n'
 ;; Set tab/spaces in each language mode
 ;;(setq-default indent-tabs-mode t)   ; use space
-;;(setq-default tab-width 4)
+
+;; https://dougie.io/emacs/indentation/
+;; Create a variable for our preferred tab width
+(setq custom-tab-width 2)
+(setq-default tab-width 4)
+;; Two callable functions for enabling/disabling tabs in Emacs
+(defun disable-tabs () (setq indent-tabs-mode nil))
+(defun enable-tabs  ()
+  (local-set-key (kbd "TAB") 'tab-to-tab-stop)
+  (setq indent-tabs-mode t)
+  (setq tab-width custom-tab-width))
+
+;; Hooks to Enable Tabs
+(add-hook 'prog-mode-hook 'enable-tabs)
+;; Hooks to Disable Tabs
+(add-hook 'lisp-mode-hook 'disable-tabs)
+(add-hook 'emacs-lisp-mode-hook 'disable-tabs)
+
+;; Make the backspace properly erase the tab instead of
+;; removing 1 space at a time.
+(setq backward-delete-char-untabify-method 'hungry)
+
+;; WARNING: This will change your life
+;; (OPTIONAL) Visualize tabs as a pipe character - "|"
+;; This will also show trailing characters as they are useful to spot.
+(setq whitespace-style '(face tabs tab-mark trailing))
+(custom-set-faces
+ '(whitespace-tab ((t (:foreground "#636363")))))
+(setq whitespace-display-mappings
+  '((tab-mark 9 [124 9] [92 9]))) ; 124 is the ascii ID for '\|'
+(global-whitespace-mode) ; Enable whitespace mode everywhere
+;; end tabs config
+
 (defalias 'yes-or-no-p #'y-or-n-p)
 
 ;; enable line numbers
